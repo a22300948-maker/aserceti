517bedebe4baa2057a1bfdf65daaa68109fffb87
a22300948-maker <a22300948@ceti.mx>
2026-02-20 11:33:20 -0600

    Ya funciona el inicio de sesión, pero aún no se ha implementado la funcionalidad de cerrar sesión. Además, se han realizado algunos cambios en la pantalla principal del alumno para mostrar su nombre después de iniciar sesión. Se han corregido algunos errores menores en el código y se han actualizado las dependencias del proyecto.

--- src/app/frontend/pantalla-principal-alumno/pantalla-principal-alumno.ts
+++ src/app/frontend/pantalla-principal-alumno/pantalla-principal-alumno.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pantalla-principal-alumno',
   imports: [],
-  templateUrl: './pantalla-principal-alumno.html',
   styleUrl: './pantalla-principal-alumno.css',
 })
 export class PantallaPrincipalAlumno {
